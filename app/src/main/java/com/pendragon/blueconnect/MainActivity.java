8a295851f307ddf89fa63ded574e1fd3e1bf2155
Marvalero <chicemav@gmail.com>
2015-03-30 15:01:03 +0200

    Adding Navigation Drawer

--- app/src/main/java/com/pendragon/blueconnect/MainActivity.java
+++ app/src/main/java/com/pendragon/blueconnect/MainActivity.java
@@ -4,14 +4,44 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.pendragon.blueconnect.utils.DrawerItem;
+import com.pendragon.blueconnect.utils.DrawerListAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
+
+    private DrawerLayout drawerLayout;
+    private ListView     drawerList;
+    private String[] tagTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tagTitles = getResources().getStringArray(R.array.menu_items);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.drawer_list);
+        drawerList.setAdapter(new ArrayAdapter<String>(
+                this,
+                R.layout.drawer_list_item,
+                getResources().getStringArray(R.array.menu_items)));
+
+        //Nueva lista de drawer items
+        ArrayList<DrawerItem> items = new ArrayList<DrawerItem>();
+        items.add(new DrawerItem(tagTitles[0],R.drawable.profile));
+        items.add(new DrawerItem(tagTitles[1],R.drawable.search));
+
+        // Relacionar el adaptador y la escucha de la lista del drawer
+        drawerList.setAdapter(new DrawerListAdapter(this, items));
+
     }
 
 
@@ -37,3 +67,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
