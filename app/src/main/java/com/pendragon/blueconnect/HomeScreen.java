2e996d91148f22a72938dfea281756aaa7638616
Maria Valero <chimichurri_cereza@live.com>
2015-05-10 01:14:35 +0200

    Changing bluetooth part 2

---
+++ app/src/main/java/com/pendragon/blueconnect/HomeScreen.java
@@ -0,0 +1,39 @@
+package com.pendragon.blueconnect;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.pendragon.blueconnect.BluetoothChat;
+
+
+public class HomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        // Wereate a new thread to sleep
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    //Llamo a la nueva actividad
+                    //startActivity recibe por parametro un objeto del tipo Intent
+                    //El Intent recibibe por parametro el NAME de la actividad que vamos a invocar
+                    //Es el mismo que colocamos en el manifiesto
+                    Intent bluetoothChat;
+                    bluetoothChat = new Intent("com.pendragon.blueconnect");
+                    startActivity(bluetoothChat);
+                    finish();
+                }
+            }
+        };
+        //ejecuto el thread
+        timer.start();
+    }
+
+}
---
+++ app/src/main/java/com/pendragon/blueconnect/StoreInformation.java
@@ -0,0 +1,8 @@
+package com.pendragon.blueconnect;
+
+/**
+ * Created by JMPostigo on 23/3/15.
+ */
+public class StoreInformation {
+}
+    
